867bca50081728a340f8e656a61ffe1787f0af39
SMEH25 <smehendale@imsa.edu>
2025-12-20 00:09:18 -0600

    created thread safe queue template

---
+++ include/thread_safe_queue.hpp
@@ -0,0 +1,34 @@
+#include <condition_variable>
+#include <iostream>
+#include <mutex>
+#include <queue>
+
+template <typename T>
+class ThreadSafeQueue {
+    private:
+        std::queue<T> queue;
+        std::mutex mtx;
+        std::condition_variable c_var;
+
+
+    public:
+        void push(T item){
+            std::unique_lock<std::mutex> lock(mtx);
+
+            queue.push(item);
+
+            c_var.notify_one();
+        }
+
+        T pop(){
+            std::unique_lock<std::mutex> lock(mtx);
+            
+            cv.wait(lock, [this]{ return !queue.empty(); });
+
+            T item = queue.front();
+            queue.pop();
+
+            return item;
+        }
+
+};
