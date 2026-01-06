2cffe816bf9272795bc75b0d68c7bafdb2812611
SMEH25 <smehendale@imsa.edu>
2026-01-06 17:10:43 -0600

    updated core to follow new architectural design where input stream pushes raw json to thread safe string queue and order generators work in parallel to covert the json to order objects

--- src/market_exchange_core.cpp
+++ src/market_exchange_core.cpp
@@ -5,6 +5,7 @@
 #include "order.hpp"
 #include "thread_safe_queue.hpp"
 #include "id_generator.hpp"
+#include "order_generator.hpp"
 
 using namespace ex;
 
@@ -24,22 +25,39 @@ void printOrder(const Order& o) {
 int main() {
     std::cout << "--- Initializing Market Exchange Core ---" << std::endl;
 
-
+    ThreadSafeQueue<std::string> rawQueue;
     ThreadSafeQueue<Order> orderQueue;
     IdGenerator id_generator;
+    const std::string inbound_port = "5555";
+    const std::string outbound_port = "5556";
+    const int num_json_parsing_threads = 8;
 
-    InputStream inputProcessor(&orderQueue, &id_generator, "5555", "5556");
+    InputStream inputProcessor(&rawQueue, inbound_port);
 
     // create and detach thread
     std::thread inputThread(&InputStream::startListening, &inputProcessor);
     inputThread.detach();
+
+    // parellelize json parsing
+    std::vector<std::unique_ptr<OrderGenerator>> workers;
+    for (int i = 0; i < num_json_parsing_threads; ++i) {
+        // Each worker handles JSON parsing and ID generation
+        workers.push_back(std::make_unique<OrderGenerator>(
+            &rawQueue, &orderQueue, &id_generator, outbound_port
+        ));
+        
+        // Launch worker in its own thread
+        std::thread([&worker = workers.back()]() {
+            worker->run();
+        }).detach();
+    }
 
     std::cout << "[CORE] Exchange is LIVE. Waiting for orders..." << std::endl;
 
     // main processing loop
     while (true) {
         Order o = orderQueue.pop();
-
+        std::cout << "[DEBUG] Raw Queue Size: " << rawQueue.size() << " | Orders Parsed: " << o.internal_order_id << std::endl;
         printOrder(o); // placeholder for now, matching engine and order books will be used here
 
     }
